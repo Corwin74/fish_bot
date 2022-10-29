d7fcd67737c2a15c0d54b23c58f19dff5e3b1509
Samokhin Alexander <icorwin74@gmail.com>
2022-10-29 16:59:35 +0300

    Add cart support

--- motlin_api.py
+++ motlin_api.py
@@ -1,4 +1,5 @@
 import requests
+from environs import Env
 
 
 def get_token(client_id):
@@ -10,11 +11,50 @@ def get_token(client_id):
     response = requests.post(
                     'https://api.moltin.com/oauth/access_token',
                     data=request_body,
-                    timeout=30,
+                    timeout=10,
     )
     response.raise_for_status()
     return response.json().get('access_token')
 
 
+def get_products(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(
+                            'https://api.moltin.com/catalog/products',
+                            headers=headers,
+    )
+    response.raise_for_status()
+    return response.text
+
+
+def get_cart(access_token, cart_id='abc'):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    url = f'https://api.moltin.com/v2/carts/{cart_id}'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.text
+
+
+def add_product_to_cart(access_token, product_sku, quantity, cart_id='abc'):
+    headers = {
+                "Authorization": f"Bearer {access_token}",
+                "Content-Type": "application/json"
+    }
+    url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
+    json_data = {
+                    "data": {
+                        "sku": product_sku,
+                        "type": "cart_item",
+                        "quantity": quantity,
+                    }
+    }
+    response = requests.post(url, headers=headers, json=json_data)
+    response.raise_for_status()
+    return response.text
+
+
 if __name__ == "__main__":
-    print(get_token("659175c46f466a17552820f18bb9199b8876154946"))
+    env = Env()
+    env.read_env()
+    motlin_client_id = env('MOTLIN_CLIENT_ID')
+    print(add_product_to_cart(get_token(motlin_client_id), "4", 3))
